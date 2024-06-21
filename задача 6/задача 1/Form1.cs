f9dbaba25c908545b670b35c6945a431ef42eba1
Vladimir <117316977+LySpacy@users.noreply.github.com>
2024-06-21 08:24:32 +0300

    Add files via upload

---
+++ задача 6/задача 1/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace задача_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox12.Text = "0";
+            textBox11.Text = "50";
+            textBox10.Text = "0";
+            textBox9.Text = "50";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox9.Visible = true;
+            textBox10.Visible = true;
+            textBox11.Visible = true;
+            textBox12.Visible = true;
+            chart1.Visible = true;
+
+            chart1.Series.Clear(); chart1.ChartAreas.Clear();
+            chart1.ChartAreas.Add(new ChartArea());
+
+            Series series = new Series();
+
+
+            series.ChartType = SeriesChartType.Spline;
+            series.Points.Clear();
+
+            series.BorderWidth = 2;
+
+            var xmin = Double.Parse(textBox12.Text);
+            var xmax = Double.Parse(textBox11.Text);
+            var ymin = Double.Parse(textBox10.Text);
+            var ymax = Double.Parse(textBox9.Text);
+
+            chart1.ChartAreas[0].AxisX.MajorGrid.Enabled = false;
+            chart1.ChartAreas[0].AxisY.MajorGrid.Enabled = false;
+            chart1.ChartAreas[0].AxisX.Crossing = 0;
+            chart1.ChartAreas[0].AxisY.Crossing = 0;
+            chart1.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
+            chart1.ChartAreas[0].AxisY.ScaleView.Zoomable = true;
+            chart1.ChartAreas[0].AxisX.Minimum = xmin;
+            chart1.ChartAreas[0].AxisX.Maximum = xmax;
+            chart1.ChartAreas[0].AxisY.Minimum = ymin;
+            chart1.ChartAreas[0].AxisY.Maximum = ymax;
+            chart1.ChartAreas[0].AxisX.Interval = 1;
+            chart1.ChartAreas[0].AxisX.LabelStyle.Interval = 5;
+            chart1.ChartAreas[0].AxisY.Interval = 1;
+            chart1.ChartAreas[0].AxisY.LabelStyle.Interval = 5;
+            chart1.ChartAreas[0].AxisX.MajorGrid.LineColor = System.Drawing.Color.Gray;
+            chart1.ChartAreas[0].AxisY.MajorGrid.LineColor = System.Drawing.Color.Gray;
+            chart1.ChartAreas[0].AxisY.ScaleView.ZoomReset();
+
+            var step = Double.Parse(textBox3.Text);
+            var speed = Double.Parse(textBox1.Text);
+            var length = Double.Parse(textBox2.Text);
+            var k = 0.0;
+
+            var c = speed / Math.Sqrt(9.8 * length);
+
+            for (double x = 0; x <= 1 / step; x += step)
+            {
+                series.Points.AddXY(c, k);
+
+                c = c - step * Math.Sin(k);
+                k = k + step * c;
+            }
+
+            chart1.Series.Add(series);
+        }
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 45 && textBox1.SelectionStart == 0) {; }
+            else
+            {
+                if (e.KeyChar == '.') e.KeyChar = ',';
+                if (e.KeyChar != 13 && e.KeyChar != 44 && e.KeyChar != 8 && (e.KeyChar < 48 || e.KeyChar > 57))
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 45 && textBox2.SelectionStart == 0) {; }
+            else
+            {
+                if (e.KeyChar == '.') e.KeyChar = ',';
+                if (e.KeyChar != 13 && e.KeyChar != 44 && e.KeyChar != 8 && (e.KeyChar < 48 || e.KeyChar > 57))
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 45 && textBox3.SelectionStart == 0) {; }
+            else
+            {
+                if (e.KeyChar == '.') e.KeyChar = ',';
+                if (e.KeyChar != 13 && e.KeyChar != 44 && e.KeyChar != 8 && (e.KeyChar < 48 || e.KeyChar > 57))
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void textBox9_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 45 && textBox9.SelectionStart == 0) {; }
+            else
+            {
+                if (e.KeyChar == '.') e.KeyChar = ',';
+                if (e.KeyChar != 13 && e.KeyChar != 44 && e.KeyChar != 8 && (e.KeyChar < 48 || e.KeyChar > 57))
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+        private void textBox10_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 45 && textBox10.SelectionStart == 0) {; }
+            else
+            {
+                if (e.KeyChar == '.') e.KeyChar = ',';
+                if (e.KeyChar != 13 && e.KeyChar != 44 && e.KeyChar != 8 && (e.KeyChar < 48 || e.KeyChar > 57))
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+        private void textBox11_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 45 && textBox11.SelectionStart == 0) {; }
+            else
+            {
+                if (e.KeyChar == '.') e.KeyChar = ',';
+                if (e.KeyChar != 13 && e.KeyChar != 44 && e.KeyChar != 8 && (e.KeyChar < 48 || e.KeyChar > 57))
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+        private void textBox12_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 45 && textBox12.SelectionStart == 0) {; }
+            else
+            {
+                if (e.KeyChar == '.') e.KeyChar = ',';
+                if (e.KeyChar != 13 && e.KeyChar != 44 && e.KeyChar != 8 && (e.KeyChar < 48 || e.KeyChar > 57))
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox9.Visible = true;
+            textBox10.Visible = true;
+            textBox11.Visible = true;
+            textBox12.Visible = true;
+            chart1.Visible = true;
+
+            chart1.Series.Clear(); chart1.ChartAreas.Clear();
+            chart1.ChartAreas.Add(new ChartArea());
+
+            Series series = new Series();
+            Series series1 = new Series();
+            Series series2 = new Series();
+
+            series.ChartType = SeriesChartType.Spline;
+            series.Points.Clear();
+
+            series1.ChartType = SeriesChartType.Spline;
+            series1.Points.Clear();
+
+            series2.ChartType = SeriesChartType.Spline;
+            series2.Points.Clear();
+
+            series.BorderWidth = 2;
+
+            var xmin = Double.Parse(textBox12.Text);
+            var xmax = Double.Parse(textBox11.Text);
+            var ymin = Double.Parse(textBox10.Text);
+            var ymax = Double.Parse(textBox9.Text);
+
+            chart1.ChartAreas[0].AxisX.MajorGrid.Enabled = false;
+            chart1.ChartAreas[0].AxisY.MajorGrid.Enabled = false;
+
+            chart1.ChartAreas[0].AxisX.Crossing = 0;
+            chart1.ChartAreas[0].AxisY.Crossing = 0;
+
+            chart1.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
+            chart1.ChartAreas[0].AxisY.ScaleView.Zoomable = true;
+            chart1.ChartAreas[0].AxisX.Minimum = -1;
+            chart1.ChartAreas[0].AxisX.Maximum = 1; 
+            chart1.ChartAreas[0].AxisY.Minimum = ymin; 
+            chart1.ChartAreas[0].AxisY.Maximum = ymax; 
+            chart1.ChartAreas[0].AxisX.Interval = 1;
+            chart1.ChartAreas[0].AxisX.LabelStyle.Interval = 5;
+            chart1.ChartAreas[0].AxisY.Interval = 1;
+            chart1.ChartAreas[0].AxisY.LabelStyle.Interval = 5;
+            chart1.ChartAreas[0].AxisX.MajorGrid.LineColor = System.Drawing.Color.Gray;
+            chart1.ChartAreas[0].AxisY.MajorGrid.LineColor = System.Drawing.Color.Gray;
+            chart1.ChartAreas[0].AxisY.ScaleView.ZoomReset();
+
+            var step = Double.Parse(textBox3.Text);
+            var speed = Double.Parse(textBox1.Text);
+            var length = Double.Parse(textBox2.Text);
+            var k = 0.0;
+
+            var c = speed / Math.Sqrt(9.8 * length);
+
+            for (double x = 0; x <= xmax; x += step)
+            {
+                series.Points.AddXY(x, c);
+                series1.Points.AddXY(x, k);
+
+                c = c - step * Math.Sin(k);
+                k = k + step * c;
+            }
+
+            chart1.Series.Add(series);
+            chart1.Series.Add(series1);
+
+        }
+        
+    }
+}
